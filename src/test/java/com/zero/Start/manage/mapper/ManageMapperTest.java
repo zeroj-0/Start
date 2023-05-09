56ecddb673de080ea154e812d0ae6a254ed1c807
zeroj-0 <83074108+zeroj-0@users.noreply.github.com>
2023-05-09 17:18:39 +0900

    Revert "마이타비스 설정 개쩔음!!! 나랑 비슷한 실력"

--- src/test/java/com/zero/Start/manage/mapper/ManageMapperTest.java
+++ src/test/java/com/zero/Start/manage/mapper/ManageMapperTest.java
@@ -1,8 +1,6 @@
 package com.zero.Start.manage.mapper;
 
 import com.zero.Start.manage.domain.Manager;
-import com.zero.Start.manage.domain.ManagerUseLombok;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,35 +12,12 @@ public class ManageMapperTest {
     
     @Autowired
     ManageMapper manageMapper;
-
     @Test
-    @DisplayName("관리자 구하기")
     void 관리자구하기() {
-
         List<Manager> managers = manageMapper.findAll();
         
         for(Manager manager : managers) {
             System.out.println("manager.getName() = " + manager.getName());
         }
-
     }
-
-
-    @Test
-    @DisplayName("롬복사용하기")
-    void manageUseLombok() {
-
-        ManagerUseLombok manager =
-         new ManagerUseLombok("1","2","3","4"); // @AllArgsConstructor
-
-        new ManagerUseLombok(); // 이때 기본 생성자 정의
-
-
-        manager.getPassword();   //@Getter
-        manager.setPassword("2"); //@Setter
-
-    }
-
-
-
 }
